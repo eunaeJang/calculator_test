029ebc5ce65376627d8ec1b7909e5d2d99cf733f
eunae <eunae@172.30.2.133>
2018-11-14 13:08:54 +0900

    first commit

--- Calculator/src/project/calculator/test/Main.java
+++ Calculator/src/project/calculator/test/Main.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Stack stack = new Stack();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("식을 입력하시오. : ");
 		String input = scan.next();
